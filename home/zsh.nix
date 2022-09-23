a3a4cdf6c91d8589d706f3d4aac75d01989c9dba
Jamie Davies <jamie@jagd.me>
2022-09-23 20:15:42 +0100

    Port over most of my dotfiles zsh config

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,19 +1,69 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 
-{
-  programs.zsh = let
+let
+  base16 = {
     defaultTheme = "tomorrow-night";
-  in {
+    path = inputs.base16-shell.outPath;
+  };
+
+  historyFile = "${config.home.homeDirectory}/.zsh_history";
+in {
+  programs.zsh = {
     enable = true;
     enableCompletion = true;
 
-    initExtra = let
-      base16Path = inputs.base16-shell.outPath;
-    in ''
-      BASE16_THEME_DEFAULT="${defaultTheme}"
+    history = {
+      extended = true;     # Save start/end timestamps for each command
+      ignoreDups = true;   # Remove older duplicates when new lines are added
+      ignoreSpace = true;  # Don't keep any lines starting with whitespace
+      path = historyFile;
+      save = 10000;        # Number of lines to write to history file
+      size = 10000;        # Number of lines to keep in session
+    };
+
+    envExtra = ''
+      export EDITOR=vim
+      export VISUAL=$EDITOR
+    '';
+
+    initExtra = ''
+      ########################################################################
+      # HISTORY
+      ########################################################################
+
+      # Remove unnecessary whitespace from commands when saving to history
+      setopt HIST_REDUCE_BLANKS
+
+      # When using history substitution (such as sudo !!), don't execute line
+      # directly - instead load it into prompt for editing
+      setopt HIST_VERIFY
+
+      ########################################################################
+      # COMPLETION
+      ########################################################################
+
+      unsetopt MENU_COMPLETE   # don't autoselect the first completion entry
+      setopt ALWAYS_TO_END     # always move cursor to end of word after completion
+      setopt AUTO_MENU         # show completion menu on succesive tab press
+      setopt COMPLETE_IN_WORD  # allow completing in the middle of words
+
+      # Enable menu selection
+      zstyle ':completion:*:*:*:*:*' menu select
+
+      # Try completing, in order:
+      # - case-insensitive matches
+      # - partial-word matches
+      # - substring matches
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+      ########################################################################
+      # BASE16 THEME
+      ########################################################################
+
+      BASE16_THEME_DEFAULT="${base16.defaultTheme}"
       [ -n "$PS1" ] && \
-        [ -s "${base16Path}/profile_helper.sh" ] && \
-          source "${base16Path}/profile_helper.sh"
+        [ -s "${base16.path}/profile_helper.sh" ] && \
+          source "${base16.path}/profile_helper.sh"
     '';
   };
 }
