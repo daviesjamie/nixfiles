a00e80db18eff481a11b174b512d549f0ff364d0
Jamie Davies <jamie@jagd.me>
2022-09-23 09:20:54 +0100

    Start using direnv

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ base16-shell, homeDirectory, pkgs, stateVersion, username }:
+{ base16-shell, config, homeDirectory, pkgs, stateVersion, username }:
 
 {
   home = { inherit homeDirectory stateVersion username; };
@@ -16,6 +16,22 @@
     package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
+    '';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+    stdlib = ''
+      # Store .direnv in cache instead of project dir
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "${config.xdg.cacheHome}/direnv/layouts/"
+          echo -n "$PWD" | shasum | cut -d ' ' -f 1
+        )}"
+      }
     '';
   };
 
