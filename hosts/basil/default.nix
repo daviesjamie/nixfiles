2b6af8337e96ce5d6d09e083ac6ed49d765ab837
Jamie Davies <jamie@jagd.me>
2022-09-30 09:14:56 +0100

    Add some base nix, shell and locale settings for basil

--- hosts/basil/default.nix
+++ hosts/basil/default.nix
@@ -11,11 +11,23 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
+  i18n.defaultLocale = "en_GB.UTF-8";
   time.timeZone = "Europe/London";
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 
   users.users.jagd = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJLkRU/0rnP7dYbZ/jRrl94vaDJvTi/JbwkZLDPIQOD"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
