720439f13fc183b3613d1fd11b0b00587afa0ffb
Jamie Davies <jamie@jagd.me>
2023-02-23 21:23:32 +0000

    Configure zfs auto trim, scrub and snapshots

--- hosts/basil/default.nix
+++ hosts/basil/default.nix
@@ -71,4 +71,23 @@
 
   # Only keep the last 500MiB of systemd journal.
   services.journald.extraConfig = "SystemMaxUse=500M";
+
+  # Trimming informs the underlying storage devices of all blocks in the pool
+  # which are no longer allocated and allows thinly provisioned devices to
+  # reclaim the space.
+  # In order for auto-trim to work, it must be set on the pool:
+  # `sudo zpool set autotrim=on <pool>`
+  services.zfs.trim.enable = true;
+  services.zfs.trim.interval = "weekly";
+
+  # Scrubbing examines all data in the specified pools to verify that it
+  # checksums correctly.
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoScrub.interval = "monthly";
+
+  # Snapshots are read-only copies of a filesystem taken at a moment in time.
+  # In order for auto-snapshot to work, it must be set on the dataset:
+  # `sudo zfs set com.sun:auto-snapshot=true <dataset>`
+  services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoSnapshot.monthly = 3;
 }
