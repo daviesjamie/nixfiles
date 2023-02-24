79f28ea432dd3e2ad30f61344a7c99c7ded794e5
Jamie Davies <jamie@jagd.me>
2023-02-24 08:40:42 +0000

    Run home-assistant with host networking mode
    It seems that it needs this for successful auto-discovery of devices.
    I could manually work through a list of all ports required for all of
    the types of devices I want to use, but this seems tedious.
    
    Perhaps I'll move home-assistant to a separate host to prevent port
    conflicts with any other services I want to run...

--- hosts/basil/services/home-assistant.nix
+++ hosts/basil/services/home-assistant.nix
@@ -3,8 +3,8 @@
     home-assistant = {
       autoStart = true;
       image = "ghcr.io/home-assistant/home-assistant:stable";
-      ports = [ "8123:8123" ];
       volumes = ["/persist/home-assistant/config:/config"];
+      extraOptions = ["--network=host" "--privileged"];
     };
   };
 }
