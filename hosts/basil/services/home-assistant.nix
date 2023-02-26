88c5d642cb0439ee8ade6e6f0e7c832388ba405b
Jamie Davies <jamie@jagd.me>
2023-02-26 16:45:36 +0000

    Lock down home assistant port/device usage

--- hosts/basil/services/home-assistant.nix
+++ hosts/basil/services/home-assistant.nix
@@ -3,8 +3,9 @@
     home-assistant = {
       autoStart = true;
       image = "ghcr.io/home-assistant/home-assistant:stable";
+      ports = [ "1900:1900" "1901:1901" "5353:5353" "8123:8123" "51827:51827" ];
       volumes = ["/persist/home-assistant/config:/config"];
-      extraOptions = ["--network=host" "--privileged"];
+      extraOptions = ["--device=/dev/ttyUSB0:/dev/ttyUSB0"];
     };
   };
 }
