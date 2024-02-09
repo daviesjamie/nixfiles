66eeab4901da90cf51c7ff2eb05ef06a97cd1fe9
Jamie Davies <jamie@jagd.me>
2024-02-09 23:06:07 +0000

    Enable AdGuard Home on basil

--- hosts/basil/default.nix
+++ hosts/basil/default.nix
@@ -48,6 +48,9 @@ in {
   # Only keep the last 500MiB of systemd journal.
   services.journald.extraConfig = "SystemMaxUse=500M";
 
+  networking.firewall.allowedTCPPorts = [53 80 3123];
+  networking.firewall.allowedUDPPorts = [53];
+
   # Containers
   nixfiles.containers.backend = "podman";
   nixfiles.containers.volumeBaseDir = "${persistDir}/docker-volumes";
@@ -58,11 +61,18 @@ in {
   nixfiles.paperless.consumeDir = "${persistDir}/paperless/consume";
   nixfiles.paperless.exportDir = "${persistDir}/paperless/export";
 
+  # AdGuard Home
+  nixfiles.adguardhome.enable = true;
+  nixfiles.adguardhome.port = 8001;
+
   # Caddy reverse proxy
-  networking.firewall.allowedTCPPorts = [80];
   services.caddy.enable = true;
   services.caddy.virtualHosts."paperless.jagd.me:80".extraConfig = ''
     encode gzip
     reverse_proxy http://localhost:${toString config.nixfiles.paperless.port}
+  '';
+  services.caddy.virtualHosts."dns.jagd.me:80".extraConfig = ''
+    encode gzip
+    reverse_proxy http://localhost:${toString config.nixfiles.adguardhome.port}
   '';
 }
